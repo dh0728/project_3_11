0f1bc83aa5987cfada89f789ce794d0bb4b25193
Oh-seo-jin <seojin2414@gmail.com>
2024-03-11 10:55:13 +0900

    mod routes_sj

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require("express");
 const dbConnect = require("./config/dbConnect")
 
 const server = express();
+// const server = http.createServer((req,res)=>{
+//   console.log("request received")
+// });
 
 const port = 3000;
 dbConnect();
 
-// const server = http.createServer((req,res)=>{
-//   console.log("request received")
-// });
+//routes
+server.use("/", require("./routes/loginRoutes"))
+// server.use("/home", require("./routes/contactRoutes"))
+
 
 server.listen(3000, ()=>{
   console.log("server started")
