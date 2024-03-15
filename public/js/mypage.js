14575058d5144ee9bbf788b8be66ecd5e06d9103
Oh-seo-jin <seojin2414@gmail.com>
2024-03-15 12:14:15 +0900

    mod mypage css/js id to class_sj

--- public/js/mypage.js
+++ public/js/mypage.js
@@ -1,34 +1,37 @@
-var container = document.getElementById("container");
-var modal = document.getElementById("modal");
-var modalImg = document.getElementById("modalImage");
-var modalText = document.getElementById("modalText");
+var container = document.getElementsByClassName("container");
+var modal = document.querySelector(".modal");
+var modalImg = document.querySelector(".modalImage");
+var modalText = document.querySelector(".modalText");
 
 // 클릭
-var images = document.querySelectorAll("#myPosts img");
-images.forEach(img => {
-  img.onclick = function(){
+var images = document.querySelectorAll("#myPosts .postImg");
+console.log(">>",images)
+images.forEach((img) => {
+  img.onclick = function () {
+    modal.style.border = "3px solid black";
     modal.style.display = "block";
     modalImg.src = this.src;
     modalText.innerHTML = this.getAttribute("postText");
-  }
+  };
 });
 
 // 닫기
-var span = document.getElementsByClassName("close")[0];
+var span = document.querySelector(".close");
 
-span.onclick = function() { 
+span.onclick = function () {
   modal.style.display = "none";
-}
+};
 
-// window.onclick = function(event) {
-//   if (event.target == container) {
+// window.onclick = function (event) {
+//   if (event.target == modal) {
 //     modal.style.display = "none";
 //   }
-// }
+// };
 
 var deleteButton = document.getElementById("deleteButton");
 
-deleteButton.onclick = function() {
-  confirm('정말 삭제하시겠습니까?')
-  
-}
+deleteButton.onclick = function () {
+  if (confirm("정말 삭제하시겠습니까?")) {
+    alert("삭제되었습니다.");
+  }
+};
